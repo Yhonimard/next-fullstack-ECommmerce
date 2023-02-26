08ec24b3a95d4719dd80ed835dbac6416d986c9b
Yhonimard <yhoni2103@gmail.com>
2023-02-26 18:21:11 +0700

    feat/adding-api-products

---
+++ pages/api/products/users/signup.js
@@ -0,0 +1,5 @@
+/**
+ *
+ *@param {import("next").NextApiRequest} req
+ *@param {import("next").NextApiResponse} req
+ * */
---
+++ src/models/ProductsModels.js
@@ -0,0 +1,10 @@
+import { model, models, Schema } from "mongoose";
+
+const product = new Schema({
+  name: { type: String, required: true },
+  category: { type: String, required: true },
+  price: { type: Number, required: true },
+});
+
+const productSchema = models.Products || model("Products", product);
+export default productSchema;
