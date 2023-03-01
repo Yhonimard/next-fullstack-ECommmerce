df75e776cb28ea42421cffaf982fc254a9fa62c6
Yhonimard <yhoni2103@gmail.com>
2023-03-02 04:43:02 +0700

    add form and validate form

--- src/models/AuthSchema.js
+++ src/models/AuthSchema.js
@@ -1,13 +1,13 @@
-import { useForm } from "react-hook-form";
-import yup from "yup";
+import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
+import { useForm } from "react-hook-form";
 
-export default function LoginSchema() {
-  const LoginSchema = yup.object({
-    username: yup.string().min().max().required(),
+export const SignupSchema = () => {
+  const schema = yup.object({
+    username: yup.string().min(4).required(),
     age: yup.number().min(15).max(70).positive().integer().required(),
     email: yup.string().email().required(),
-    address: yup.string().length(20).required(),
+    address: yup.string().min(5).required(),
     password: yup.string().min(6).max(20).required(),
     confirmPassword: yup
       .string()
@@ -15,13 +15,34 @@ export default function LoginSchema() {
       .required(),
   });
 
-  const { register, handleSubmit } = useForm({
-    resolver: yupResolver(LoginSchema),
-  });
-
-  return {
-    LoginSchema,
+  const {
     register,
     handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: yupResolver(schema) });
+
+  return {
+    register,
+    handleSubmit,
+    errors,
   };
-}
+};
+
+export const LoginSchema = () => {
+  const schema = yup.object({
+    email: yup.string().email().required(),
+    password: yup.string().min(6).max(20).required(),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: yupResolver(schema) });
+
+  return {
+    register,
+    handleSubmit,
+    errors,
+  };
+};
